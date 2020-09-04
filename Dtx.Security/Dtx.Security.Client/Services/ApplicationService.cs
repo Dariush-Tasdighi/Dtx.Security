8fd8cc99b6b7780738d1938d009339af1f68c6d3
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2020-09-04 20:48:55 +0430

    Making some changes!

--- Dtx.Security/Dtx.Security.Client/Services/ApplicationService.cs
+++ Dtx.Security/Dtx.Security.Client/Services/ApplicationService.cs
@@ -33,13 +33,80 @@ namespace Dtx.Security.Client.Services
 				await Client.PostAsJsonAsync
 				(requestUri: RequestUri, value: viewModel);
 
+			response.EnsureSuccessStatusCode();
+
+			if (response.IsSuccessStatusCode)
+			{
+				// New Solution
+				Models.Application result =
+					await response.Content.ReadFromJsonAsync<Models.Application>();
+
+				return result;
+				// /New Solution
+
+				// Old Solution
+				//string data =
+				//	await response.Content.ReadAsStringAsync();
+
+				//Models.Application result =
+				//	System.Text.Json.JsonSerializer.Deserialize<Models.Application>(data);
+				// /Old Solution
+			}
+
+			return null;
+
+
+			//try
+			//{
+			//	return await response.Content.ReadFromJsonAsync<User>();
+			//}
+			//catch (NotSupportedException) // When content type is not valid
+			//{
+			//	Console.WriteLine("The content type is not supported.");
+			//}
+			//catch (JsonException) // Invalid JSON
+			//{
+			//	Console.WriteLine("Invalid JSON.");
+			//}
+
+
+
 			//var result =
 			//	response.Content.ReadFromJsonAsync<Models.Application>().Result;
 
-			Models.Application result =
-				await response.Content.ReadFromJsonAsync<Models.Application>();
+			//Models.Application result =
+			//	await response.Content.ReadFromJsonAsync<Models.Application>();
 
-			return result;
+			//System.Text.Json.JsonSerializerOptions jsonSerializerOptions =
+			//	new System.Text.Json.JsonSerializerOptions
+			//	{
+			//		MaxDepth = 5,
+			//	};
+
+			//Models.Application result =
+			//	await response.Content.ReadFromJsonAsync<Models.Application>(options: jsonSerializerOptions);
+
+
+			return null;
+			//return result;
+
+			//try
+			//{
+			//	// HTTP POST
+			//	var response = await client.PostAsJsonAsync(requestUri, postValues);
+			//	response.EnsureSuccessStatusCode(); // Throw if not a success code.
+			//	if (response.IsSuccessStatusCode)
+			//	{
+			//		var result = await response.Content.ReadAsStringAsync();
+			//		Debug.WriteLine(result);
+			//	}
+			//}
+			//catch (HttpRequestException e)
+			//{
+			//	// Handle exception.
+			//	Debug.WriteLine(e.ToString());
+			//	throw;
+			//}
 		}
 	}
 }
