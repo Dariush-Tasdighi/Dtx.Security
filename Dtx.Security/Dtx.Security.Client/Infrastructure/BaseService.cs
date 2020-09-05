8b3eb654eef9c25519ded3ae0179308afadef238
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2020-09-05 12:42:37 +0430

    Making some changes!

--- Dtx.Security/Dtx.Security.Client/Infrastructure/BaseService.cs
+++ Dtx.Security/Dtx.Security.Client/Infrastructure/BaseService.cs
@@ -28,5 +28,205 @@ namespace Infrastructure
 		protected System.Net.Http.HttpClient Client { get; set; }
 
 		protected System.Text.Json.JsonSerializerOptions JsonOptions { get; set; }
+
+		protected virtual async System.Threading.Tasks.Task<O> GetAsync<O>()
+		{
+			System.Net.Http.HttpResponseMessage response = null;
+
+			try
+			{
+				response =
+					await Client.GetAsync(requestUri: RequestUri);
+
+				response.EnsureSuccessStatusCode();
+
+				if (response.IsSuccessStatusCode)
+				{
+					try
+					{
+						O result =
+							await response.Content.ReadFromJsonAsync<O>();
+
+						return result;
+					}
+					// When content type is not valid
+					catch (System.NotSupportedException)
+					{
+						System.Console.WriteLine("The content type is not supported.");
+					}
+					// Invalid JSON
+					catch (System.Text.Json.JsonException)
+					{
+						System.Console.WriteLine("Invalid JSON.");
+					}
+				}
+			}
+			catch (System.Net.Http.HttpRequestException ex)
+			{
+				System.Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				response.Dispose();
+			}
+
+			return default;
+		}
+
+		protected virtual async System.Threading.Tasks.Task<O> PostAsync<I, O>(I viewModel)
+		{
+			System.Net.Http.HttpResponseMessage response = null;
+
+			try
+			{
+				response =
+					await Client.PostAsJsonAsync
+					(requestUri: RequestUri, value: viewModel);
+
+				response.EnsureSuccessStatusCode();
+
+				if (response.IsSuccessStatusCode)
+				{
+					try
+					{
+						//System.Text.Json.JsonSerializerOptions jsonSerializerOptions =
+						//	new System.Text.Json.JsonSerializerOptions
+						//	{
+						//		MaxDepth = 5,
+						//	};
+
+						//O result =
+						//	await response.Content.ReadFromJsonAsync<O>(options: jsonSerializerOptions);
+
+
+
+						// New Solution
+						O result =
+							await response.Content.ReadFromJsonAsync<O>();
+
+						return result;
+						// /New Solution
+
+						// Old Solution
+						//string data =
+						//	await response.Content.ReadAsStringAsync();
+
+						//O result =
+						//	System.Text.Json.JsonSerializer.Deserialize<O>(data);
+						// /Old Solution
+					}
+					// When content type is not valid
+					catch (System.NotSupportedException)
+					{
+						System.Console.WriteLine("The content type is not supported.");
+					}
+					// Invalid JSON
+					catch (System.Text.Json.JsonException)
+					{
+						System.Console.WriteLine("Invalid JSON.");
+					}
+				}
+			}
+			catch (System.Net.Http.HttpRequestException ex)
+			{
+				System.Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				response.Dispose();
+				//response = null;
+			}
+
+			return default;
+		}
+
+		protected virtual async System.Threading.Tasks.Task<O> PutAsync<I, O>(I viewModel)
+		{
+			System.Net.Http.HttpResponseMessage response = null;
+
+			try
+			{
+				response =
+					await Client.PutAsJsonAsync
+					(requestUri: RequestUri, value: viewModel);
+
+				response.EnsureSuccessStatusCode();
+
+				if (response.IsSuccessStatusCode)
+				{
+					try
+					{
+						O result =
+							await response.Content.ReadFromJsonAsync<O>();
+
+						return result;
+					}
+					// When content type is not valid
+					catch (System.NotSupportedException)
+					{
+						System.Console.WriteLine("The content type is not supported.");
+					}
+					// Invalid JSON
+					catch (System.Text.Json.JsonException)
+					{
+						System.Console.WriteLine("Invalid JSON.");
+					}
+				}
+			}
+			catch (System.Net.Http.HttpRequestException ex)
+			{
+				System.Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				response.Dispose();
+			}
+
+			return default;
+		}
+
+		protected virtual async System.Threading.Tasks.Task<O> DeleteAsync<O>()
+		{
+			System.Net.Http.HttpResponseMessage response = null;
+
+			try
+			{
+				response =
+					await Client.DeleteAsync(requestUri: RequestUri);
+
+				response.EnsureSuccessStatusCode();
+
+				if (response.IsSuccessStatusCode)
+				{
+					try
+					{
+						O result =
+							await response.Content.ReadFromJsonAsync<O>();
+
+						return result;
+					}
+					// When content type is not valid
+					catch (System.NotSupportedException)
+					{
+						System.Console.WriteLine("The content type is not supported.");
+					}
+					// Invalid JSON
+					catch (System.Text.Json.JsonException)
+					{
+						System.Console.WriteLine("Invalid JSON.");
+					}
+				}
+			}
+			catch (System.Net.Http.HttpRequestException ex)
+			{
+				System.Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				response.Dispose();
+			}
+
+			return default;
+		}
 	}
 }
--- Dtx.Security/Dtx.Security.Client/Services/ApplicationService.cs
+++ Dtx.Security/Dtx.Security.Client/Services/ApplicationService.cs
@@ -18,9 +18,12 @@ namespace Dtx.Security.Client.Services
 			GetAsync()
 		{
 			var result =
-				await Client.GetFromJsonAsync
-				<System.Collections.Generic.IList<Models.Application>>
-				(requestUri: RequestUri);
+				await GetAsync<System.Collections.Generic.IList<Models.Application>>();
+
+			//var result =
+			//	await Client.GetFromJsonAsync
+			//	<System.Collections.Generic.IList<Models.Application>>
+			//	(requestUri: RequestUri);
 
 			return result;
 		}
@@ -29,84 +32,10 @@ namespace Dtx.Security.Client.Services
 			<Models.Application>
 			PostAsync(ViewModels.Applications.CreateViewModel viewModel)
 		{
-			var response =
-				await Client.PostAsJsonAsync
-				(requestUri: RequestUri, value: viewModel);
+			var result =
+				await PostAsync<ViewModels.Applications.CreateViewModel, Models.Application>(viewModel);
 
-			response.EnsureSuccessStatusCode();
-
-			if (response.IsSuccessStatusCode)
-			{
-				// New Solution
-				Models.Application result =
-					await response.Content.ReadFromJsonAsync<Models.Application>();
-
-				return result;
-				// /New Solution
-
-				// Old Solution
-				//string data =
-				//	await response.Content.ReadAsStringAsync();
-
-				//Models.Application result =
-				//	System.Text.Json.JsonSerializer.Deserialize<Models.Application>(data);
-				// /Old Solution
-			}
-
-			return null;
-
-
-			//try
-			//{
-			//	return await response.Content.ReadFromJsonAsync<User>();
-			//}
-			//catch (NotSupportedException) // When content type is not valid
-			//{
-			//	Console.WriteLine("The content type is not supported.");
-			//}
-			//catch (JsonException) // Invalid JSON
-			//{
-			//	Console.WriteLine("Invalid JSON.");
-			//}
-
-
-
-			//var result =
-			//	response.Content.ReadFromJsonAsync<Models.Application>().Result;
-
-			//Models.Application result =
-			//	await response.Content.ReadFromJsonAsync<Models.Application>();
-
-			//System.Text.Json.JsonSerializerOptions jsonSerializerOptions =
-			//	new System.Text.Json.JsonSerializerOptions
-			//	{
-			//		MaxDepth = 5,
-			//	};
-
-			//Models.Application result =
-			//	await response.Content.ReadFromJsonAsync<Models.Application>(options: jsonSerializerOptions);
-
-
-			return null;
-			//return result;
-
-			//try
-			//{
-			//	// HTTP POST
-			//	var response = await client.PostAsJsonAsync(requestUri, postValues);
-			//	response.EnsureSuccessStatusCode(); // Throw if not a success code.
-			//	if (response.IsSuccessStatusCode)
-			//	{
-			//		var result = await response.Content.ReadAsStringAsync();
-			//		Debug.WriteLine(result);
-			//	}
-			//}
-			//catch (HttpRequestException e)
-			//{
-			//	// Handle exception.
-			//	Debug.WriteLine(e.ToString());
-			//	throw;
-			//}
+			return result;
 		}
 	}
 }
