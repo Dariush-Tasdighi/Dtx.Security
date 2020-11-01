0df51bc2d004f08f1f574fc1befb15cb0f688866
saeed <saeed_shirzadian@yahoo.com>
2020-11-01 19:51:28 +0330

    ساخت ریپازیتوری لاگ وضعیت دسترسی

--- Dtx.Security/Data/IUserRepository.cs
+++ Dtx.Security/Data/IUserRepository.cs
@@ -2,10 +2,52 @@
 {
 	public interface IUserRepository : Base.IRepository<Models.User>
 	{
+		/// <summary>
+		/// فراخوانی کاربر برحسب نام کاربر
+		/// </summary>
+		/// <param name="username"></param>
+		/// <returns></returns>
 		Models.User GetByUsername(string username);
 
+		/// <summary>
+		/// فراخوانی کاربر برحسب نام کاربر
+		/// </summary>
+		/// <param name="username"></param>
+		/// <returns></returns>
 		System.Threading.Tasks.Task<Models.User> GetByUsernameAsync(string username);
 
+		/// <summary>
+		/// لیست کاربران فعال
+		/// </summary>
+		/// <returns></returns>
 		System.Collections.Generic.List<Models.User> GetActive();
-	}
+
+		/// <summary>
+		/// فراخوانی کاربر برحسب ایمیل آدرس
+		/// </summary>
+		/// <param name="emailAddress"></param>
+		/// <returns></returns>
+		Models.User GetByEmailAddress(string emailAddress);
+
+		/// <summary>
+		/// فراخوانی کاربر برحسب ایمیل آدرس
+		/// </summary>
+		/// <param name="emailAddress"></param>
+		/// <returns></returns>
+		System.Threading.Tasks.Task<Models.User> GetByEmailAddressAsync(string emailAddress);
+
+		/// <summary>
+		/// فراخوانی کاربر برحسب کد ملی
+		/// </summary>
+		/// <param name="nationalCode"></param>
+		/// <returns></returns>
+		Models.User GetByNationalCode(string nationalCode);
+
+		/// <summary>
+		/// فراخوانی کاربر برحسب کد ملی
+		/// </summary>
+		/// <param name="nationalCode"></param>
+		/// <returns></returns>
+		System.Threading.Tasks.Task<Models.User> GetByNationalCodeAsync(string nationalCode);
+}
 }
---
+++ Dtx.Security/Data/Repository/UserRepository.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CheckPermissionLogRepository : Repository<Models.CheckPermissionLog>, Data.Repository.ICheckPermissionLogRepository
+    {
+        internal CheckPermissionLogRepository(DatabaseContext databaseContext) : base(databaseContext)
+        {
+        }
+
+        /// <summary>
+        /// فراخوانی لیست لاگ های شده و نشده برحسب کاربر
+        /// </summary>
+        /// <param name="userId">شناسه کاربر</param>
+        /// <param name="issuccessful">وضعیت موفقیت</param>
+        /// <returns></returns>
+        public System.Collections.Generic.List<Models.CheckPermissionLog> GetByUserId(System.Guid userId, bool? issuccessful = null)
+        {
+            //var result =
+            //	DbSet
+            //	.Where(current => string.Compare(current.UserId, UserId, true) == 0)
+            //	.FirstOrDefault();
+            var result =
+                DbSet
+                .Where(current => current.UserId == userId &&
+                    (issuccessful == null || current.Issuccessful == issuccessful.GetValueOrDefault()))
+                .ToList();
+
+            return result;
+        }
+
+        /// <summary>
+        /// فراخوانی لیست لاگ های شده و نشده برحسب کاربر
+        /// </summary>
+        /// <param name="userId">شناسه کاربر</param>
+        /// <param name="issuccessful">وضعیت موفقیت</param>
+        /// <returns></returns>
+        public System.Threading.Tasks.Task<System.Collections.Generic.List<Models.CheckPermissionLog>> GetByUserIdAsync(System.Guid userId, bool? issuccessful = null)
+        {
+
+            var result =
+                DbSet
+                .Where(current => current.UserId == userId &&
+                    (issuccessful == null || current.Issuccessful == issuccessful.GetValueOrDefault()))
+                .ToListAsync();
+
+            return result;
+        }
+
+        /// <summary>
+        /// فراخوانی لیست لاگ های شده و نشده برحسب کاربر
+        /// </summary>
+        /// <param name="userId">شناسه کاربر</param>
+        /// <param name="entityPathName">مسیر اکشن</param>
+        /// <returns></returns>
+        public System.Collections.Generic.List<Models.CheckPermissionLog> GetByUserIdAndEEntityPathName(System.Guid userId, string entityPathName)
+        {
+            //var result =
+            //	DbSet
+            //	.Where(current => string.Compare(current.UserId, UserId, true) == 0)
+            //	.FirstOrDefault();
+            var result =
+                DbSet
+                .Where(current => current.UserId == userId &&
+                    current.EntityPathName.ToLower() == entityPathName.ToLower())
+                .ToList();
+
+            return result;
+        }
+
+        /// <summary>
+        /// فراخوانی لیست لاگ های شده و نشده برحسب کاربر
+        /// </summary>
+        /// <param name="userId">شناسه کاربر</param>
+        /// <param name="entityPathName">مسیر اکشن</param>
+        /// <returns></returns>
+        public System.Threading.Tasks.Task<System.Collections.Generic.List<Models.CheckPermissionLog>> GetByUserIdAndEEntityPathNameAsync(System.Guid userId, string entityPathName)
+        {
+
+            var result =
+                DbSet
+                .Where(current => current.UserId == userId &&
+                    current.EntityPathName.ToLower() == entityPathName.ToLower())
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
