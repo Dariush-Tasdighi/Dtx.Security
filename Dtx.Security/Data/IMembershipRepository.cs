ea17ae10f6b2644c1e8599e1b032c78834303094
farzad fathi <farzadfathi3737@gmail.com>
2020-11-23 10:50:38 +0330

    Add Data For Models

---
+++ Dtx.Security/Data/IMembershipRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Data
+{
+	public interface IMembershipRepository : Base.IRepository<Models.Membership>
+	{
+		System.Collections.Generic.List<Models.ApplicationGroup> GetUserApplicationGroupsByUserId(System.Guid id);
+		System.Collections.Generic.List<Models.User> GetApplicationGroupUsersByApplicationGroupId(System.Guid id);
+	}
+}
---
+++ Dtx.Security/Data/UserValidIPRepository.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+
+namespace Data
+{
+	public class UserValidIPRepository : Repository<Models.UserValidIP>, IUserValidIPRepository
+	{
+		internal UserValidIPRepository(DatabaseContext databaseContext) : base(databaseContext)
+		{
+		}
+
+		public System.Collections.Generic.List<Models.UserValidIP> GetActive()
+		{
+			var result =
+				DbSet
+				.Where(current => current.IsActive)
+				.ToList()
+				;
+
+			return result;
+		}
+
+		public System.Collections.Generic.List<Models.UserValidIP> GetByUserId(System.Guid id)
+		{
+			var result =
+				DbSet
+				.Where(current => current.UserId == id)
+				.ToList()
+				;
+
+			return result;
+		}
+
+		public System.Collections.Generic.List<Models.UserValidIP> GetByUsername(string username)
+		{
+			if (string.IsNullOrWhiteSpace(username))
+			{
+				return null;
+			}
+
+			var result =
+				DbSet
+				.Where(current => current.User.Username.ToLower() == username.ToLower())
+				.ToList()
+				;
+
+			return result;
+		}
+
+		public System.Collections.Generic.List<Models.UserValidIP> GetActiveByUserId(System.Guid id)
+		{
+			var result =
+				DbSet
+				.Where(current => current.UserId == id)
+				.Where(current => current.IsActive)
+				.ToList()
+				;
+
+			return result;
+		}
+
+		public System.Collections.Generic.List<Models.UserValidIP> GetActiveByUsername(string username)
+		{
+			if (string.IsNullOrWhiteSpace(username))
+			{
+				return null;
+			}
+
+			var result =
+				DbSet
+				.Where(current => current.User.Username.ToLower() == username.ToLower())
+				.Where(current => current.IsActive)
+				.ToList()
+				;
+
+			return result;
+		}
+	}
+}
